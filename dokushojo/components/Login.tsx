8f9a5429bd819eb11f42f0346b99469e1af24762
joaoAraki27 <gamerrealms765@gmail.com>
2024-08-03 02:56:36 +0900

    Front end  taking form

--- dokushojo/components/Login.tsx
+++ dokushojo/components/Login.tsx
@@ -1,51 +1,54 @@
-
 import React, { useState } from 'react';
 import { GoogleLogin, googleLogout, GoogleOAuthProvider } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 const clientId = "11471929898-7c74bgss3h1c1f4q13bas5isbo74edfs.apps.googleusercontent.com";
 
 function Login() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [information, setInformation] = useState<any>('')
+  const [information, setInformation] = useState<any>(null);
+  const navigate = useNavigate();
 
-  
   const handleLogout = () => {
-    setIsLoggedIn(false)
+    setIsLoggedIn(false);
     googleLogout();
+  };
+
+  const handleSuccess = (credentialResponse: any) => {
+    const decoded = jwtDecode(credentialResponse?.credential);
+    setInformation(decoded);
+    setIsLoggedIn(true);
+    navigate("/dojo");
+    console.log(decoded);
+    console.log(credentialResponse);
+  };
+
+  const handleError = () => {
+    console.log('Login Failed');
   };
 
   return (
     <GoogleOAuthProvider clientId={clientId}>
-
-    <div className="container">
-      {!isLoggedIn ? (
-        <GoogleLogin
-        onSuccess={credentialResponse => {
-          const decoded = jwtDecode(credentialResponse?.credential);
-          setInformation(decoded)
-          setIsLoggedIn(true)
-          console.log(decoded);
-          console.log(credentialResponse);
-          
-        }}
-        
-        onError={() => {
-          console.log('Login Failed');
-        }}
-      />
-      
-      ) : (
-        
-        <div style={{ textAlign: 'center' }}>
-          <h1>User Information</h1>
-          {<img className="profile" src={information.picture} alt="Profile" />}
-          <p>Name: {information.name}</p>
-          <p>Email: {information.email}</p>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      )}
-    </div>
+      <img src='/hello.svg' className="welcome-message" alt='Welcome' />
+      <div className="google-login-button">
+        {!isLoggedIn ? (
+          <GoogleLogin
+            onSuccess={handleSuccess}
+            onError={handleError}
+          />
+        ) : (
+          <div style={{ textAlign: 'center' }}>
+            <h1>User Information</h1>
+            {information?.picture && (
+              <img className="profile" src={information.picture} alt="Profile" />
+            )}
+            <p>Name: {information?.name}</p>
+            <p>Email: {information?.email}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        )}
+      </div>
     </GoogleOAuthProvider>
   );
 }
--- dokushojo/src/App.tsx
+++ dokushojo/src/App.tsx
@@ -1,22 +1,41 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import Login from "../components/Login";
+import Dojo from "./Dojo";
 
 function App() {
+  const location = useLocation();
+  const isDojoPage = location.pathname === "/dojo";
 
   return (
-    <>
-      <div className="container text-center mw-100">
-        <div className="row vh-100">
-          <div className="col-9 pt-2"></div>
-          <div className="col mx-auto pt-2">
-            <div className="mx-auto">
-              <Login></Login>
-            </div>
+    <div className="container text-center mw-100">
+      <div className="row vh-100">
+        {!isDojoPage && (
+          <div className="col-9 pt-2">
+            <img src="circle copiar.svg" className="circle-image spin-image" alt="Circle" />
+            <img src="Dokushojo.svg" className="circle-image center-image" alt="Dokushojo" />
+            <img src="Dokushojo-title.svg" className="circle-title center-title" alt="Dokushojo" />
+          </div>
+        )}
+        <div className="col mx-auto pt-2">
+          <div className="mx-auto">
+            <Routes>
+              <Route path="/" element={<Login />} />
+            </Routes>
           </div>
         </div>
+            <Routes>
+            <Route path="/dojo" element={<Dojo />} />
+            </Routes>
       </div>
-    </>
+    </div>
   );
 }
 
-export default App;
+export default function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
