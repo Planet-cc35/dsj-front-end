8f355ad8fd4c642b0e47c94d21ed3027834df975
ljbroersen <lj.broersen@gmail.com>
2024-08-02 15:57:40 +0900

    fix: oversaw merge conflicts

--- dokushojo/src/main.tsx
+++ dokushojo/src/main.tsx
@@ -1,15 +1,8 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import "../scss/custom.scss";
->>>>>>> Stashed changes
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
